48424789f6da3ffdf7e372acda178815f5e15afc
randegang <randegang@baidu.com>
2020-05-26 22:56:31 +0800

    close nox

--- site-lisp/config/init-nox.el
+++ site-lisp/config/init-nox.el
@@ -92,7 +92,7 @@
                ;; 'python-mode-hook
                ;; 'ruby-mode-hook
                ;; 'java-mode-hook
-               'sh-mode-hook
+               ;; 'sh-mode-hook
                ;; 'php-mode-hook
                ;; 'c-mode-common-hook
                ;; 'c-mode-hook
