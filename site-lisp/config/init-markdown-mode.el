4852e691b6bd750f0c9f2f07f50ebf2858df225f
LambdaRan <2861429309@qq.com>
2020-10-31 21:39:57 +0800

    Update submodules

--- site-lisp/config/init-markdown-mode.el
+++ site-lisp/config/init-markdown-mode.el
@@ -84,53 +84,6 @@
 (require 'grip-mode)
 ;;; Code:
 
-;; (setq markdown-enable-wiki-links t
-;;       markdown-italic-underscore t
-;;       markdown-asymmetric-header t
-;;       markdown-make-gfm-checkboxes-buttons t
-;;       markdown-gfm-uppercase-checkbox t
-;;       markdown-fontify-code-blocks-natively t
-
-;;       markdown-content-type "application/xhtml+xml"
-;;       markdown-css-paths '("https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.min.css"
-;;                            "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/github.min.css")
-;;       markdown-xhtml-header-content "
-;; <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
-;; <style>
-;; body {
-;;   box-sizing: border-box;
-;;   max-width: 740px;
-;;   width: 100%;
-;;   margin: 40px auto;
-;;   padding: 0 10px;
-;; }
-;; </style>
-;; <link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/default.min.css'>
-;; <script src='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/highlight.min.js'></script>
-;; <script>
-;; document.addEventListener('DOMContentLoaded', () => {
-;;   document.body.classList.add('markdown-body');
-;;   document.querySelectorAll('pre code').forEach((code) => {
-;;     if (code.className != 'mermaid') {
-;;       hljs.highlightBlock(code);
-;;     }
-;;   });
-;; });
-;; </script>
-;; <script src='https://unpkg.com/mermaid@8.4.8/dist/mermaid.min.js'></script>
-;; <script>
-;; mermaid.initialize({
-;;   theme: 'default',  // default, forest, dark, neutral
-;;   startOnLoad: true
-;; });
-;; </script>
-;; "
-;;       markdown-gfm-additional-languages "Mermaid")
-
-;; ;; `multimarkdown' is necessary for `highlight.js' and `mermaid.js'
-;; (when (executable-find "multimarkdown")
-;;   (setq markdown-command "multimarkdown"))
-
 ;; https://emacs-china.org/t/markdown-org-grip-mode/10262/48
 ;; Preview via `grip'
 ;; Make a keybinding: `C-c C-c g'
@@ -150,5 +103,4 @@
 ;; (add-hook 'org-mode-hook #'grip-mode)
 
 (provide 'init-markdown-mode)
-
 ;;; init-markdown-mode.el ends here
