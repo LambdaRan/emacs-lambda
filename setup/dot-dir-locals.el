3ddf29939969a82c1dc8ef652a76429714facec5
LambdaRan <2861429309@qq.com>
2021-07-22 00:42:06 +0800

    add setup directory

---
+++ setup/dot-dir-locals.el
@@ -0,0 +1,7 @@
+((nil . ((ffip-project-root . "f:/xxx")
+         ;; only search files with following extensions
+         ;; (ffip-patterns . ("*.lua" "*.cpp" "*.h"))
+         (counsel-etags-extra-tags-files . ("f:/xxx/TAGS"))
+	     (color-rg-project-root . "f:/xxx")
+         (indent-tabs-mode t)
+         )))
