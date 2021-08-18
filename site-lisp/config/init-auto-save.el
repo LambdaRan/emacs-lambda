5ef08ecff9b3410aa10078b150eb3e15571729be
LambdaRan <2861429309@qq.com>
2021-08-18 13:15:00 +0800

    auto-save buffer loal

--- site-lisp/config/init-auto-save.el
+++ site-lisp/config/init-auto-save.el
@@ -97,7 +97,7 @@
 
 (auto-save-enable)
 (setq auto-save-silent t)
-(setq auto-save-delete-trailing-whitespace t)
+(setq-local auto-save-delete-trailing-whitespace t)
 (setq auto-save-idle 1)                 ; idle 2s then to save
 
 (provide 'init-auto-save)
