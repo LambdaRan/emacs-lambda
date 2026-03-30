952c485c8a99db7be5c0207fe19e5b70a855d928
LambdaRan <2861429309@qq.com>
2026-03-30 11:55:56 +0800

    fix markdown treesitter version

--- site-lisp/config/init-treesit.el
+++ site-lisp/config/init-treesit.el
@@ -1,88 +1,12 @@
 ;;; init-treesit.el --- Config for treesit   -*- lexical-binding: t; -*-
 
-;; Filename: init-treesit.el
-;; Description: Config for treesit
-;; Author: Andy Stewart <lazycat.manatee@gmail.com>
-;; Maintainer: Andy Stewart <lazycat.manatee@gmail.com>
-;; Copyright (C) 2023, Andy Stewart, all rights reserved.
-;; Created: 2023-02-04 12:37:41
-;; Version: 0.1
-;; Last-Updated: 2023-02-04 12:37:41
-;;           By: Andy Stewart
-;; URL: https://www.github.org/manateelazycat/init-treesit
-;; Keywords:
-;; Compatibility: GNU Emacs 30.0.50
-;;
-;; Features that might be required by this library:
-;;
-;;
-;;
-
-;;; This file is NOT part of GNU Emacs
-
-;;; License
-;;
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
-;; Floor, Boston, MA 02110-1301, USA.
-
-;;; Commentary:
-;;
-;; Config for treesit
-;;
-
-;;; Installation:
-;;
-;; Put init-treesit.el to your load-path.
-;; The load-path is usually ~/elisp/.
-;; It's set in your ~/.emacs like this:
-;; (add-to-list 'load-path (expand-file-name "~/elisp"))
-;;
-;; And the following to your ~/.emacs startup file.
-;;
-;; (require 'init-treesit)
-;;
-;; No need more.
-
-;;; Customize:
-;;
-;;
-;;
-;; All of the above can customize by:
-;;      M-x customize-group RET init-treesit RET
-;;
-
-;;; Change log:
-;;
-;; 2023/02/04
-;;      * First released.
-;;
-
-;;; Acknowledgements:
-;;
-;;
-;;
-
-;;; TODO
-;;
-;;
-;;
-
 ;;; Require
 (require 'treesit)
 
 ;;; Code:
+
+;; 执行以下函数确定ABI版本，当前是14
+;; (treesit-library-abi-version)
 
 ;; Customize treesit grammer load path.
 ;; (setq treesit-extra-load-path (list (concat lazycat-emacs-root-dir "/treesit-grammer")))
@@ -111,8 +35,8 @@
         (json . ("https://github.com/tree-sitter/tree-sitter-json"))
         (lua . ("https://github.com/Azganoth/tree-sitter-lua"))
         (make . ("https://github.com/alemuller/tree-sitter-make"))
-        (markdown . ("https://github.com/tree-sitter-grammars/tree-sitter-markdown" "v0.5.0" "tree-sitter-markdown/src"))
-        (markdown-inline . ("https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown-inline/src"))
+        (markdown . ("https://github.com/tree-sitter-grammars/tree-sitter-markdown" "v0.4.1" "tree-sitter-markdown/src"))
+        (markdown-inline . ("https://github.com/tree-sitter-grammars/tree-sitter-markdown" "v0.4.0" "tree-sitter-markdown-inline/src"))
         (ocaml . ("https://github.com/tree-sitter/tree-sitter-ocaml" nil "ocaml/src"))
         (org . ("https://github.com/milisims/tree-sitter-org"))
         (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.23.5"))
